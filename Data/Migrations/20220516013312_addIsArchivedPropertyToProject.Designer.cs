5b380415af072bb8d63b2e0ddc14b36980f8acff
hhb <hhbryan@126.com>
2022-05-15 21:57:41 -0400

    add Archive project feature and role authorization

--- Data/Migrations/20220516013312_addIsArchivedPropertyToProject.Designer.cs
+++ Data/Migrations/20220516013312_addIsArchivedPropertyToProject.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SD_125_BugTracker.Data;
 
@@ -11,9 +12,10 @@ using SD_125_BugTracker.Data;
 namespace SD_125_BugTracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220516013312_addIsArchivedPropertyToProject")]
+    partial class addIsArchivedPropertyToProject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
